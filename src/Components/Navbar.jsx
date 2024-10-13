2fc256ad3e5ee2179cdab50994893ee53ceab5fd
svimaladhithan <vimaladhithan007@gmail.com>
2024-10-13 06:42:33 +0530

    feat: removed instafrom portfolio

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,7 +13,6 @@ const Navbar = () => {
             <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
                 <motion.button whileHover={{scale:1.2}} ><a href='https://www.linkedin.com/in/vimaladhithan-s/' target='_blank'><FaLinkedin /></a></motion.button>
                 <motion.button whileHover={{scale:1.2}}><a href='https://github.com/svimaladhithan' target='_blank'><FaGithub /></a></motion.button>
-                <motion.button whileHover={{scale:1.2}}><a href='https://www.instagram.com/vimaladhithan_s/' target='_blank'><FaInstagram /></a></motion.button>
             </div>
         </nav>
     );
